e719dddbf2be44129050d451801f71cd48e84573
BhoomikaAIML <nsb207242@gmail.com>
2025-11-10 16:36:29 +0530

    Create arithematic.py

---
+++ arithematic.py
@@ -0,0 +1,12 @@
+a = int(input("enter the num1:"))
+b =int(input("enter the num2:"))
+addition = a + b
+sub = a - b
+multiplication = a * b
+div = a / b
+floor_division = a//b
+modulus_division = a%b
+print(addition,sub,multiplication,div,floor_division,modulus_division )
+enter the num1:5
+enter the num2:2
+7 3 10 2.5 2 1
