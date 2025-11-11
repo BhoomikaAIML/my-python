dea9833b71e3da869717df48cb96a50450400044
BhoomikaAIML <nsb207242@gmail.com>
2025-11-11 15:14:47 +0530

    Create positive_negative.py

---
+++ positive_negative.py
@@ -0,0 +1,10 @@
+#positive and negative 
+a=int(input("enter the number:"))
+if a>=0 :
+  print("it is a possitive number")
+else:
+  print("it is a negative number")
+  #output
+enter the number:-2
+it is a negative number
+
