a963ac34cfdc62752f0b8ddc785969c47f3b7e26
BhoomikaAIML <nsb207242@gmail.com>
2025-11-11 15:39:29 +0530

    Create vowel_consonent.py

---
+++ vowel_consonent.py
@@ -0,0 +1,9 @@
+#vowel and consonant
+character = input("enter the character:")
+if character in "a e i o u" or "A E I O U" :
+  print("the character is vowel")
+else:
+  print("the character is consonant")
+  #output
+enter the character:A
+the character is vowel
