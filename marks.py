630237be93b86c4def504303c59ee56c2c332736
BhoomikaAIML <nsb207242@gmail.com>
2025-11-11 15:12:28 +0530

    Create marks.py

---
+++ marks.py
@@ -0,0 +1,15 @@
+  # marks
+  a = int(input("enter your marks:"))
+  if a>=90:
+    print("excellent")
+  elif a>=70:
+    print("good")
+  elif a>=60:
+    print("average")
+  elif a>=50:
+    print("just pass")
+  else:
+    print("fail")
+#output
+enter your marks:90
+excellent
