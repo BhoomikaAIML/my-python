c1c27fd82e65a39f627fc7107ccc48bf554b97de
BhoomikaAIML <nsb207242@gmail.com>
2025-11-11 15:35:26 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+#for loop
+for a in range(1,10):
+  if a%2==0:
+    print("even number")
+  else:
+    print("odd number")
+    #output
+    odd number
+    even number
+    odd number
+    even number
+    odd number
+    even number
+    odd number
+    even number
+    odd number
