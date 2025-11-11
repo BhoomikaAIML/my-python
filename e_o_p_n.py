d154f8ae804ae314a25ed0ae5bf5fb3dbcaeeae0
BhoomikaAIML <nsb207242@gmail.com>
2025-11-11 15:32:25 +0530

    Create e_o_p_n.py

---
+++ e_o_p_n.py
@@ -0,0 +1,17 @@
+#even,odd,positive and negative
+num = int(input("enter the number:"))
+if num%2==0 and num>0:
+  print(enter the number:15
+the num is divisible both 3 and 5
+print("even and positive")
+elif num%2==0 and num<0:
+  print("even and negative")
+elif num%2!=0 and num>0:
+  print("odd and positive")
+elif num%2!=0 and num<0:
+  print("odd and negative")
+else:
+  print("the number is zero")
+  #output
+enter the number:24
+even and positive
