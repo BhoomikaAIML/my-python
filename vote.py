dd20605033701b00587eee904e055f154de8a017
BhoomikaAIML <nsb207242@gmail.com>
2025-11-11 15:24:25 +0530

    Create vote.py

---
+++ vote.py
@@ -0,0 +1,9 @@
+#eligble for vote
+age = int(input("enter the age:"))
+if age>=18:
+  print("eligible to vote")
+else :
+  print("not eligible to vote")
+  #output
+  enter the age:22
+eligible to vote
