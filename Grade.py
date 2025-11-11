017bb8e61b36458f1e0e9e1c3cb3dff2537f0e89
BhoomikaAIML <nsb207242@gmail.com>
2025-11-11 15:25:23 +0530

    Create Grade.py

---
+++ Grade.py
@@ -0,0 +1,13 @@
+#grade
+a = int(input("enter the marks:"))
+if a>=90 :
+  print("Grade A")
+elif a>=75 :
+  print("Grade B")
+elif a>=50 :
+  print("Grade C")
+else:
+  print("fail")
+  #output
+enter the marks:90
+Grade A
