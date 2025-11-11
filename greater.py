7c6fcc790993e43a5045e6c75af05313fe0da834
BhoomikaAIML <nsb207242@gmail.com>
2025-11-11 15:15:55 +0530

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,14 @@
+#compare the two greater number
+num1=int(input("enter the num1:"))
+num2=int(input("enter the num2:"))
+if num1>num2:
+  print("the num1 is greater")
+elif num1==num2:
+  print("both are equal")
+else:
+  print("the num2 is greater")
+  #output
+enter the num1:5
+enter the num2:10
+the num2 is greater
+
