fdf080cd6d05ecf7ca0bdfa39432234bd5619049
BhoomikaAIML <nsb207242@gmail.com>
2025-11-11 21:19:09 +0530

    Create Count_digits_of_number.py

---
+++ Count_digits_of_number.py
@@ -0,0 +1,10 @@
+#count digits of a number 
+num = int(input("Enter a number: "))
+count = 0
+while num != 0:
+    num //= 10
+    count += 1
+print("Number of digits:", count)
+#output
+Enter a number: 12345
+Number of digits: 5
