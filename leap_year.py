8bea1e307d02dd1bdb52d6b2bfcb66794a45cbad
BhoomikaAIML <nsb207242@gmail.com>
2025-11-11 15:20:52 +0530

    Create leap_year.py

---
+++ leap_year.py
@@ -0,0 +1,9 @@
+#leap year
+a= int(input("enter the year:"))
+if a%4==0 :
+  print("it is a leap year")
+else :
+  print("it is not a leap year")
+  #output
+enter the year:2025
+it is not a leap year
