d828d5b06a3b61ae43d24439aaafa00afd83ebc0
BhoomikaAIML <nsb207242@gmail.com>
2025-11-12 10:29:57 +0530

    Create formated_string1.py

---
+++ formated_string1.py
@@ -0,0 +1,7 @@
+name="Bhoomika"
+age=18
+studying= "shridevi"
+department="AIML"
+print(f"my name is :{name} and my age is : {age} and i studying in {studying}college in department of {department}")
+#output
+my name is :Bhoomika and my age is : 18 and i studying in shridevicollege in department of AIML
