361b3d60610fc3e0b5adc68da14a92093d27f523
BhoomikaAIML <nsb207242@gmail.com>
2025-11-13 09:20:42 +0530

    Create simple_interest_def.py

---
+++ simple_interest_def.py
@@ -0,0 +1,6 @@
+#simple interest
+def simple_interest(principle,rate,time):
+  return (principle*time*rate)/100
+print("the simple interest is:",simple_interest(15,45,20))
+#output
+the simple interest is: 135.0
