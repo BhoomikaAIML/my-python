99e1821454e70f9e152dbe6f4b5fbb48bf29bb90
BhoomikaAIML <nsb207242@gmail.com>
2025-11-14 12:23:39 +0530

    Create sum_average_of_list.py

---
+++ sum_average_of_list.py
@@ -0,0 +1,10 @@
+#sum and average of list element
+numbers = [10, 20, 30, 40, 50]
+
+total = sum(numbers)
+average = total / len(numbers)
+
+print("Sum:", total)
+print("Average:", average)
+#Sum: 150
+Average: 30.0
