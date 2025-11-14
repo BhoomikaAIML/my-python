3edcace35f65cd85c8d6bbe41a9d5c4e47b59425
BhoomikaAIML <nsb207242@gmail.com>
2025-11-14 12:30:26 +0530

    Create even_num.py

---
+++ even_num.py
@@ -0,0 +1,14 @@
+#even number
+numbers = [10, 15, 22, 33, 40, 55, 60]
+
+print("Even numbers:")
+for n in numbers:
+    if n % 2 == 0:
+        print(n)
+#output
+Even numbers:
+10
+22
+40
+60
+    
