78477069eed6443312f33a0f6ab5516156353dd0
BhoomikaAIML <nsb207242@gmail.com>
2025-11-14 12:33:21 +0530

    Create sort_list.py

---
+++ sort_list.py
@@ -0,0 +1,8 @@
+#sort list
+numbers = [50, 10, 40, 20, 30]
+
+print("Ascending:", sorted(numbers))
+print("Descending:", sorted(numbers, reverse=True))
+#output
+Ascending: [10, 20, 30, 40, 50]
+Descending: [50, 40, 30, 20, 10]
