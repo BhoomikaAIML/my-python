1d8168a473d5d0648136acd9b8bb67616cde0812
BhoomikaAIML <nsb207242@gmail.com>
2025-11-14 12:28:02 +0530

    Create count_list.py

---
+++ count_list.py
@@ -0,0 +1,10 @@
+#count element occurrences
+numbers = [1, 2, 3, 2, 4, 2, 5]
+
+x = int(input("Enter number to count: "))
+count = numbers.count(x)
+
+print("Count:", count)
+#output
+Enter number to count: 3
+Count: 1
