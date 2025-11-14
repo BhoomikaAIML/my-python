562a10e3a57d058af105ac71b8ada07908732c08
BhoomikaAIML <nsb207242@gmail.com>
2025-11-14 12:22:01 +0530

    Create list_of_num.py

---
+++ list_of_num.py
@@ -0,0 +1,13 @@
+#list of 5 numbers 
+numbers = []
+for i in range(5):
+    n = int(input("Enter a number: "))
+    numbers.append(n)
+print("List:", numbers)
+#output
+Enter a number: 4
+Enter a number: 5
+Enter a number: 6
+Enter a number: 7
+Enter a number: 8
+List: [4, 5, 6, 7, 8]
