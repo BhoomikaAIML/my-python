af80cd85b7d5b50b044d651d70e1564ad5636f04
BhoomikaAIML <nsb207242@gmail.com>
2025-11-14 10:17:51 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+#dictionary
+a={
+    "name" : "bhoomi",
+    "age" : 18,
+    "weight" : 39.6,
+    "department" : "aiml"
+}
+print(a)
+#output
+{'name': 'bhoomi', 'age': 18, 'weight': 39.6, 'department': 'aiml'}
